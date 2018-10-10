6a374e097fb1e1877ff7573c4c7b09c4b3133bac
Francisco Javier Caracuel Beltrán <fjcb312@gmail.com>
2018-10-10 04:46:32 +0200

    Some styles adjusted to be responsive

--- js/helper.js
+++ js/helper.js
@@ -126,12 +126,9 @@ const successDetails = (response, extraData) => {
             class="resultsImg"
             src="${response.Poster}"
             alt="${response.Title}"
-            width="200"
         />
         <div id="contentDetailContainer">
-            <div>
-                <span class="titleDetail">${response.Title} (${response.Year})</span>
-            </div>
+            <span class="titleDetail">${response.Title} (${response.Year})</span>
             <div id="durationGenreReleasedDetailContainer">
                 <span>${response.Runtime} | ${response.Genre} | ${response.Released}</span>
             </div>
@@ -139,7 +136,7 @@ const successDetails = (response, extraData) => {
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </div>
         </div>
-        <div>
+        <div id="imdbRatingContainer">
             <span class="ratingTextDetail">${response.imdbRating}</span>/10
         </div>
         `)
