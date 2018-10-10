4118621a77252354ebaf157804b5dae105573d92
Francisco Javier Caracuel Beltrán <fjcb312@gmail.com>
2018-10-09 19:54:21 -0700

    NodeJS files deleted

--- index.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const fs = require('fs');
-http.createServer(function (req, res) {
-  fs.readFile('index.html', function(err, data) {
-    res.write(data);
-    res.end();
-  });
-}).listen(8080);
